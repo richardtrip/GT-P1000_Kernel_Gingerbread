743acf3106ac12603c191e564b7f45f805d99d35
richardtrip <richard.trip@gmail.com>
2011-05-25 11:23:10 +0200

    initial release

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 3 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 PREEMPT Wed May 25 11:08:28 CEST 2011"
+#define LINUX_COMPILE_TIME "11:08:28"
+#define LINUX_COMPILE_BY "trip"
+#define LINUX_COMPILE_HOST "trip-desktop"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
